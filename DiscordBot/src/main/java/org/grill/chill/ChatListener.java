14ac7961605b6a53be424150e22cc16542f1ded3
Arnold <arnoldying825@gmail.com>
2024-01-29 00:38:52 -0800

    basic template

---
+++ DiscordBot/src/main/java/org/grill/chill/ChatListener.java
@@ -0,0 +1,16 @@
+package org.grill.chill;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ChatListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+
+        String msg = event.getMessage().getContentRaw();
+        System.out.println(msg);
+    }
+
+}
--- DiscordBot/src/main/java/org/grill/chill/Main.java
+++ DiscordBot/src/main/java/org/grill/chill/Main.java
@@ -1,7 +1,31 @@
 package org.grill.chill;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Configurations configs = new Configurations();
+            Configuration config  = configs.properties(new File("config.properties"));
+
+            JDABuilder builder = JDABuilder.createDefault(config.getString("botToken"));
+
+            builder.setActivity(Activity.playing("with your mom"));
+            builder.addEventListeners(new ChatListener());
+
+            builder.build();
+        } catch (LoginException e) {
+            throw new RuntimeException(e);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
